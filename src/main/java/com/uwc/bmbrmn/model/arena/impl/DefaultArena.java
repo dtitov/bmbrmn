50ec67212768a173d3eb7a8c1e88b0021f9c8ec3
Dmytro Titov <dmytro.titov@gmail.com>
2016-03-20 21:23:45 +0200

    Fix flaming animation.

--- src/main/java/com/uwc/bmbrmn/model/arena/impl/DefaultArena.java
+++ src/main/java/com/uwc/bmbrmn/model/arena/impl/DefaultArena.java
@@ -158,6 +158,8 @@ public class DefaultArena implements Arena {
                         anotherCell.setMined(true);
                     }
 
+                    anotherCell.setFlaming(false);
+
                     changesTracker.track(item);
                     changesTracker.track(anotherCell);
                 }

5a020c3b456f6348d2c008b983bb548cf860634f
Dmytro Titov <dmytro.titov@gmail.com>
2016-03-21 23:34:22 +0200

    Fix players head.

--- src/main/java/com/uwc/bmbrmn/logic/ChangesTracker.java
+++ src/main/java/com/uwc/bmbrmn/logic/ChangesTracker.java
@@ -1,13 +1,13 @@
 package com.uwc.bmbrmn.logic;
 
 import com.uwc.bmbrmn.model.arena.Cell;
+import com.uwc.bmbrmn.model.units.Player;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.util.Collection;
-import java.util.HashSet;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
@@ -21,7 +21,8 @@ public class ChangesTracker<T extends Cell> {
     }
 
     public Collection<T> cutSlice() {
-        Collection<T> slice = new HashSet<>(changedItems.values());
+        Collection<T> slice = new ArrayList<>(changedItems.values());
+        Collections.sort((List<T>) slice, (o1, o2) -> Boolean.compare(o1 instanceof Player, o2 instanceof Player));
         changedItems.clear();
         return slice;
     }
